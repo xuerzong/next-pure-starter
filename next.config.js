5c3d43b23c94fd16a8cf2c0b66a341a1f19063c9
xucong <xuerzong@gmail.com>
2023-02-02 18:19:09 +0800

    Feat: bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
+let nextConfig = {};
+
+nextConfig = withBundleAnalyzer(nextConfig);
+
+module.exports = nextConfig;
